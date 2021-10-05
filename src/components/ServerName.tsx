c5d0986b6a7eac208ca507fa00a6c2f99d48adaf
Fabian Vowie <fabian@vowie.me>
2021-10-05 22:35:56 +0200

    Add dropdown to server name component

--- src/components/ServerName.tsx
+++ src/components/ServerName.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useAppSelector } from '../app/hooks';
 import CreateChannel from '../features/channels/CreateChannel';
 import { selectServers } from '../features/servers/serversSlice';
+import Dropdown from './Dropdown';
+import DropdownItem from './DropdownItem';
+import {
+    TrashIcon,
+} from '@heroicons/react/solid'
 
 export default function ServerName(): JSX.Element {
     const { selectedServer } = useAppSelector(selectServers);
@@ -9,7 +14,14 @@ export default function ServerName(): JSX.Element {
     return (
         <div className="flex items-center h-12 border-b border-gray-800">
             <p className='flex-1 mx-4 text-white text-xl font-semibold select-none'>{selectedServer?.name}</p>
-            <CreateChannel selectedServer={selectedServer} />
+            <Dropdown>
+                <div className='py-1'>
+                    <CreateChannel selectedServer={selectedServer} />
+                    <DropdownItem onClick={(e) => e.preventDefault()} icon={TrashIcon}>
+                        Delete Server
+                    </DropdownItem>
+                </div>
+            </Dropdown>
         </div>
     );
 }
