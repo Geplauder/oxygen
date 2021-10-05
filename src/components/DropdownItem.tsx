84dea91516362ec413accf8c645ed24df4475a79
Fabian Vowie <fabian@vowie.me>
2021-10-05 22:35:41 +0200

    Add dropdown and dropdown item components

---
+++ src/components/DropdownItem.tsx
@@ -0,0 +1,27 @@
+import { Menu } from '@headlessui/react';
+import classNames from 'classnames';
+import React, { MouseEventHandler } from 'react';
+
+export default function DropdownItem({ children, icon, onClick }: { children: React.ReactNode, icon: React.ElementType, onClick: MouseEventHandler }): JSX.Element {
+    const iconElement = React.createElement(icon, {
+        className: 'mr-3 h-5 w-5 white group-hover:white',
+    });
+
+    return (
+        <Menu.Item>
+            {({ active }) => (
+                <a
+                    onClick={onClick}
+                    href="#"
+                    className={classNames(
+                        active ? 'bg-[#26282e] text-white' : 'text-white',
+                        'group flex items-center px-4 py-2 text-sm'
+                    )}
+                >
+                    {iconElement}
+                    {children}
+                </a>
+            )}
+        </Menu.Item>
+    );
+}
