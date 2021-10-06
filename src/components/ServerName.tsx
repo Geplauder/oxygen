e7b9219c39f7abca9e85b4bb23f86ad3cae7f94d
Fabian Vowie <fabian@vowie.me>
2021-10-06 14:10:06 +0200

    Add delete server component

--- src/components/ServerName.tsx
+++ src/components/ServerName.tsx
@@ -5,29 +5,38 @@ import { selectServers } from '../features/servers/serversSlice';
 import Dropdown from './Dropdown';
 import DropdownItem from './DropdownItem';
 import {
+    ArrowCircleLeftIcon,
     PlusIcon,
     TrashIcon,
 } from '@heroicons/react/solid'
+import DeleteServer from '../features/servers/DeleteServer';
 
 export default function ServerName(): JSX.Element {
     const { selectedServer } = useAppSelector(selectServers);
 
     const [openCreateChannel, setOpenCreateChannel] = useState(false);
+    const [openDeleteServer, setOpenDeleteServer] = useState(false);
 
     return (
         <div className="flex items-center h-12 border-b border-gray-800">
-            <p className='flex-1 mx-4 text-white text-xl font-semibold select-none'>{selectedServer?.name}</p>
+            <p className='flex-1 mx-4 text-white text-xl font-semibold select-none truncate'>{selectedServer?.name}</p>
             <Dropdown>
                 <div className='py-1'>
-                    <DropdownItem onClick={(e) => { e.preventDefault(); setOpenCreateChannel(true); }} icon={PlusIcon}>
+                    <DropdownItem onClick={(e) => setOpenCreateChannel(true)} icon={PlusIcon}>
                         Create Channel
                     </DropdownItem>
-                    <DropdownItem onClick={(e) => { }} icon={TrashIcon}>
+                    <DropdownItem onClick={(e) => setOpenDeleteServer(true)} icon={TrashIcon}>
                         Delete Server
+                    </DropdownItem>
+                </div>
+                <div className="py-1">
+                    <DropdownItem onClick={(e) => { }} icon={ArrowCircleLeftIcon}>
+                        Leave Server
                     </DropdownItem>
                 </div>
             </Dropdown>
             <CreateChannel open={openCreateChannel} setOpen={setOpenCreateChannel} selectedServer={selectedServer} />
+            <DeleteServer open={openDeleteServer} setOpen={setOpenDeleteServer} />
         </div>
     );
 }
