2603a19d785fb071456b4e4c668f0a8a729d9d28
Fabian Vowie <fabian@vowie.me>
2021-10-09 20:40:14 +0200

    Lazily load channel messages

--- src/features/messages/Messages.tsx
+++ src/features/messages/Messages.tsx
@@ -1,23 +1,27 @@
 import React, { useEffect, useRef } from 'react';
-import { useAppSelector } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import ChannelName from '../../components/ChannelName';
 import MessageBox from '../../components/MessageBox';
 import { selectChannels } from '../channels/channelsSlice';
-import { selectMessages } from './messagesSlice';
+import { getMessagesAsync, selectMessages } from './messagesSlice';
 import Message from './Message';
 
 export default function Messages(): JSX.Element {
+    const dispatch = useAppDispatch();
+
     const { selectedChannel } = useAppSelector(selectChannels);
     const { messages } = useAppSelector(selectMessages);
 
     const messagesEnd = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        if (messagesEnd.current === null) {
-            return;
+        if (selectedChannel && messages[selectedChannel.id] == null) {
+            dispatch(getMessagesAsync({ channelId: selectedChannel.id }));
         }
 
-        messagesEnd.current.scrollIntoView();
+        if (messagesEnd.current !== null) {
+            messagesEnd.current.scrollIntoView();
+        }
     }, [messages, selectedChannel]);
 
     return (
