f3b69392fc9bc2e526bf54b5b39f0596441eee9d
Fabian Vowie <fabian@vowie.me>
2021-10-09 22:02:52 +0200

    Change default focus for create channel component and handle enter key

--- src/features/channels/CreateChannel.tsx
+++ src/features/channels/CreateChannel.tsx
@@ -1,25 +1,55 @@
-import React, { Fragment, useRef, useState } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { postChannelAsync } from './channelsSlice';
-import { Server } from '../../types';
-import { selectUser } from '../user/userSlice';
+import React, { FormEvent, Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { postChannelAsync } from "./channelsSlice";
+import { Server } from "../../types";
+import { selectUser } from "../user/userSlice";
 
-export default function CreateChannel({ selectedServer, open, setOpen }: { selectedServer: Server | null, open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>> }): JSX.Element {
+export default function CreateChannel({
+    selectedServer,
+    open,
+    setOpen,
+}: {
+    selectedServer: Server | null;
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}): JSX.Element {
     const dispatch = useAppDispatch();
 
     const { user } = useAppSelector(selectUser);
 
-    const [channelName, setChannelName] = useState('New Channel');
-    const cancelButtonRef = useRef(null);
+    const [channelName, setChannelName] = useState("New Channel");
+    const inputRef = useRef(null);
 
     if (selectedServer === null || user?.id !== selectedServer.owner_id) {
         return <div />;
     }
 
+    const createChannel = () => {
+        dispatch(
+            postChannelAsync({ serverId: selectedServer?.id, name: channelName })
+        );
+        setOpen(false);
+    };
+
+    const handleSubmit = (event: React.KeyboardEvent) => {
+        if (event.key !== "Enter") {
+            return;
+        }
+
+        event.preventDefault();
+
+        createChannel();
+    };
+
     return (
         <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
+            <Dialog
+                as="div"
+                className="fixed z-10 inset-0 overflow-y-auto"
+                initialFocus={inputRef}
+                onClose={setOpen}
+            >
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <Transition.Child
                         as={Fragment}
@@ -32,7 +62,10 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                     >
                         <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
                     </Transition.Child>
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                    <span
+                        className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                        aria-hidden="true"
+                    >
                         &#8203;
                     </span>
                     <Transition.Child
@@ -48,12 +81,18 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="sm:flex sm:items-start">
                                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                                        <Dialog.Title
+                                            as="h3"
+                                            className="text-lg leading-6 font-medium text-gray-900"
+                                        >
                                             Create Channel
                                         </Dialog.Title>
                                         <div className="mt-2">
                                             <div>
-                                                <label htmlFor="channel-name" className="block text-sm font-medium text-gray-700">
+                                                <label
+                                                    htmlFor="channel-name"
+                                                    className="block text-sm font-medium text-gray-700"
+                                                >
                                                     Channel Name
                                                 </label>
                                                 <div className="mt-1">
@@ -62,8 +101,10 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                                                         name="channel-name"
                                                         id="channel-name"
                                                         className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                                        ref={inputRef}
                                                         value={channelName}
-                                                        onChange={e => setChannelName(e.target.value)}
+                                                        onChange={(e) => setChannelName(e.target.value)}
+                                                        onKeyDown={(e) => handleSubmit(e)}
                                                     />
                                                 </div>
                                             </div>
@@ -75,7 +116,7 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                                 <button
                                     type="button"
                                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                    onClick={() => { dispatch(postChannelAsync({ serverId: selectedServer?.id, name: channelName })); setOpen(false); }}
+                                    onClick={createChannel}
                                 >
                                     Create
                                 </button>
@@ -83,7 +124,6 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                                     type="button"
                                     className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                                     onClick={() => setOpen(false)}
-                                    ref={cancelButtonRef}
                                 >
                                     Cancel
                                 </button>
@@ -93,5 +133,5 @@ export default function CreateChannel({ selectedServer, open, setOpen }: { selec
                 </div>
             </Dialog>
         </Transition.Root>
-    )
+    );
 }
