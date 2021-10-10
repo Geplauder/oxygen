01fe776e81f98e09b56f4c58b390289c099ce378
Fabian Vowie <fabian@vowie.me>
2021-10-10 18:28:16 +0200

    Redesign modals

--- src/features/channels/CreateChannel.tsx
+++ src/features/channels/CreateChannel.tsx
@@ -57,7 +57,7 @@ export default function CreateChannel({
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
                     >
-                        <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                        <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity" />
                     </Transition.Child>
                     <span
                         className="hidden sm:inline-block sm:align-middle sm:h-screen"
@@ -74,13 +74,13 @@ export default function CreateChannel({
                         leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                         leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
-                        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div className="inline-block align-bottom bg-main rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                            <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="sm:flex sm:items-start">
                                     <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                         <Dialog.Title
                                             as="h3"
-                                            className="text-lg leading-6 font-medium text-gray-900"
+                                            className="text-lg leading-6 font-medium text-white"
                                         >
                                             Create Channel
                                         </Dialog.Title>
@@ -88,7 +88,7 @@ export default function CreateChannel({
                                             <div>
                                                 <label
                                                     htmlFor="channel-name"
-                                                    className="block text-sm font-medium text-gray-700"
+                                                    className="block text-sm font-medium text-gray-100"
                                                 >
                                                     Channel Name
                                                 </label>
@@ -97,7 +97,7 @@ export default function CreateChannel({
                                                         type="text"
                                                         name="channel-name"
                                                         id="channel-name"
-                                                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                                                        className="bg-main-dark text-white shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-main-black rounded-md"
                                                         ref={inputRef}
                                                         value={channelName}
                                                         onChange={(e) => setChannelName(e.target.value)}
@@ -109,7 +109,7 @@ export default function CreateChannel({
                                     </div>
                                 </div>
                             </div>
-                            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                            <div className="px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                                 <button
                                     type="button"
                                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
