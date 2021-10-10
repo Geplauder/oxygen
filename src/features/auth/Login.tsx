3807ca1cbed691b0c29661b916ac7ca40e205f23
Fabian Vowie <fabian@vowie.me>
2021-10-10 18:18:15 +0200

    Redesign login page

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -40,18 +40,13 @@ export default function Login(): JSX.Element {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen flex items-center justify-center bg-main py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
-                    <img
-                        className="mx-auto h-12 w-auto"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                        alt=""
-                    />
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-                    <p className="mt-2 text-center text-sm text-gray-600">
+                    <h2 className="mt-6 text-center text-3xl font-extrabold text-white">Sign in to your account</h2>
+                    <p className="mt-2 text-center text-sm text-gray-100">
                         Or{' '}
-                        <Link to="/register" className="font-medium text-indigo-600 hover:text-indigo-500">
+                        <Link to="/register" className="font-medium text-indigo-500 hover:text-indigo-400">
                             Sign up
                         </Link>
                     </p>
@@ -82,7 +77,7 @@ export default function Login(): JSX.Element {
                                 type="email"
                                 autoComplete="email"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 bg-main-dark text-white border border-main-black placeholder-gray-400 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Email address"
                                 value={email}
                                 onChange={e => setEmail(e.target.value)}
@@ -98,7 +93,7 @@ export default function Login(): JSX.Element {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                                className="appearance-none rounded-none relative block w-full px-3 py-2 bg-main-dark text-white border border-main-black placeholder-gray-400 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                                 placeholder="Password"
                                 value={password}
                                 onChange={e => setPassword(e.target.value)}
