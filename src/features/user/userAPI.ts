ea7cc558575bfaa311ec55473cf95a97a1924432
Fabian Vowie <fabian@vowie.me>
2021-10-12 21:25:25 +0200

    Add user update api route

--- src/features/user/userAPI.ts
+++ src/features/user/userAPI.ts
@@ -4,3 +4,12 @@ import { User } from '../../types';
 export function fetchUser(): Promise<AxiosResponse<User>> {
     return axios.get('users');
 }
+
+export function postUpdateUser({ name, email, password, currentPassword }: { name?: string, email?: string, password?: string, currentPassword: string }): Promise<AxiosResponse> {
+    return axios.post('users', {
+        name,
+        email,
+        password,
+        current_password: currentPassword,
+    });
+}
