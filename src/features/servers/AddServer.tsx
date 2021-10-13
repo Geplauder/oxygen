f3bb9508b83d3b52b8d600704e1e5a9268e948dd
Fabian Vowie <fabian@vowie.me>
2021-10-13 21:50:02 +0200

    Use dedicated modal component for server modals

--- src/features/servers/AddServer.tsx
+++ src/features/servers/AddServer.tsx
@@ -1,6 +1,7 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog } from '@headlessui/react';
 import { PlusSmIcon } from '@heroicons/react/solid';
-import React, { Fragment, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { Modal } from '../../components/Modal';
 import Tooltip from '../../components/Tooltip';
 import CreateServer from './CreateServer';
 import JoinServer from './JoinServer';
@@ -23,76 +24,47 @@ export default function AddServer(): JSX.Element {
             </Tooltip>
             <CreateServer open={openCreateServer} setOpen={setOpenCreateServer} />
             <JoinServer open={openJoinServer} setOpen={setOpenJoinServer} />
-            <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
-                    <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100"
-                            leaveTo="opacity-0"
-                        >
-                            <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity" />
-                        </Transition.Child>
-
-                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                            &#8203;
-                        </span>
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <div className="inline-block align-bottom bg-main rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                                <div>
-                                    <div className="mt-3 text-center sm:mt-5">
-                                        <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
-                                            Add Server
-                                        </Dialog.Title>
-                                        <div className="mt-2">
-                                            <p className="text-sm text-gray-100">
-                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo
-                                                pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-3 sm:gap-3 sm:grid-flow-row-dense">
-                                    <button
-                                        type="button"
-                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-1 sm:text-sm"
-                                        onClick={() => { setOpen(false); setOpenCreateServer(true); }}
-                                    >
-                                        Create
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
-                                        onClick={() => { setOpen(false); setOpenJoinServer(true); }}
-                                    >
-                                        Join
-                                    </button>
-                                    <button
-                                        type="button"
-                                        className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-3 sm:text-sm"
-                                        onClick={() => setOpen(false)}
-                                        ref={cancelButtonRef}
-                                    >
-                                        Cancel
-                                    </button>
-                                </div>
+            <Modal open={open} setOpen={setOpen} initialFocus={cancelButtonRef}>
+                <div className='px-4 pt-5 pb-4'>
+                    <div>
+                        <div className="mt-3 text-center sm:mt-5">
+                            <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
+                                Add Server
+                            </Dialog.Title>
+                            <div className="mt-2">
+                                <p className="text-sm text-gray-100">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo
+                                    pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.
+                                </p>
                             </div>
-                        </Transition.Child>
+                        </div>
                     </div>
-                </Dialog>
-            </Transition.Root>
+                    <div className="mt-5 sm:mt-6 sm:grid sm:grid-cols-3 sm:gap-3 sm:grid-flow-row-dense">
+                        <button
+                            type="button"
+                            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-1 sm:text-sm"
+                            onClick={() => { setOpen(false); setOpenCreateServer(true); }}
+                        >
+                            Create
+                        </button>
+                        <button
+                            type="button"
+                            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:col-start-2 sm:text-sm"
+                            onClick={() => { setOpen(false); setOpenJoinServer(true); }}
+                        >
+                            Join
+                        </button>
+                        <button
+                            type="button"
+                            className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:col-start-3 sm:text-sm"
+                            onClick={() => setOpen(false)}
+                            ref={cancelButtonRef}
+                        >
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </Modal>
         </>
     )
 }
--- src/features/servers/DeleteServer.tsx
+++ src/features/servers/DeleteServer.tsx
@@ -1,7 +1,8 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog } from '@headlessui/react';
 import { ExclamationIcon } from '@heroicons/react/outline';
-import React, { Fragment, useRef } from 'react';
+import React, { useRef } from 'react';
 import { useAppDispatch } from '../../app/hooks';
+import { ActionModal } from '../../components/Modal';
 import { Server } from '../../types';
 import { deleteServerAsync } from './serversSlice';
 
@@ -14,72 +15,29 @@ export default function DeleteServer({ selectedServer, open, setOpen }: { select
         return <div />;
     }
 
-    return (
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={setOpen}>
-                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Dialog.Overlay className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity" />
-                    </Transition.Child>
+    const deleteServer = () => {
+        dispatch(deleteServerAsync({ serverId: selectedServer.id }));
+        setOpen(false);
+    };
 
-                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                        &#8203;
-                    </span>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        enterTo="opacity-100 translate-y-0 sm:scale-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                    >
-                        <div className="inline-block align-bottom bg-main rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
-                            <div className="sm:flex sm:items-start">
-                                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-500 sm:mx-0 sm:h-10 sm:w-10">
-                                    <ExclamationIcon className="h-6 w-6 text-red-100" aria-hidden="true" />
-                                </div>
-                                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
-                                        Delete Server
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-100">
-                                            Are you sure you want to delete this server? All of your data will be permanently removed
-                                            from our servers forever. This action cannot be undone.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                                <button
-                                    type="button"
-                                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                    onClick={() => { dispatch(deleteServerAsync({ serverId: selectedServer.id })); setOpen(false); }}
-                                >
-                                    Delete
-                                </button>
-                                <button
-                                    type="button"
-                                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
-                                    onClick={() => setOpen(false)}
-                                    ref={cancelButtonRef}
-                                >
-                                    Cancel
-                                </button>
-                            </div>
-                        </div>
-                    </Transition.Child>
+    return (
+        <ActionModal open={open} setOpen={setOpen} initialFocus={cancelButtonRef} actionName='Delete' onAction={deleteServer}>
+            <div className="px-4 pt-5 pb-4 sm:flex sm:items-start">
+                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-500 sm:mx-0 sm:h-10 sm:w-10">
+                    <ExclamationIcon className="h-6 w-6 text-red-100" aria-hidden="true" />
                 </div>
-            </Dialog>
-        </Transition.Root>
+                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-white">
+                        Delete Server
+                    </Dialog.Title>
+                    <div className="mt-2">
+                        <p className="text-sm text-gray-100">
+                            Are you sure you want to delete this server? All of your data will be permanently removed
+                            from our servers forever. This action cannot be undone.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </ActionModal>
     );
 }
