a085662911375803d5cf6061a988036bf9bd0bfb
Fabian Vowie <fabian@vowie.me>
2021-10-15 23:27:54 +0200

    Use settings button component for user settings logout

--- src/components/buttons/Buttons.tsx
+++ src/components/buttons/Buttons.tsx
@@ -13,7 +13,7 @@ const Button: FunctionComponent<ButtonProps> = ({ className, children, onClick, 
             type='button'
             className={classNames(
                 className,
-                'inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+                'inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
                 {
                     'cursor-not-allowed opacity-50': isLoading,
                 }
--- src/pages/UserSettings.tsx
+++ src/pages/UserSettings.tsx
@@ -3,7 +3,7 @@ import { LogoutIcon, CogIcon } from '@heroicons/react/outline';
 import { Route, useHistory, useRouteMatch } from 'react-router';
 import { useAppDispatch } from '../app/hooks';
 import { invalidateToken } from '../features/auth/authSlice';
-import { Settings, SettingsContent, SettingsLink, SettingsNavigation, SettingsNavigationBottom, SettingsNavigationTop } from '../components/settings/BaseSettings';
+import { Settings, SettingsButton, SettingsContent, SettingsLink, SettingsNavigation, SettingsNavigationBottom, SettingsNavigationTop } from '../components/settings/BaseSettings';
 import UserInfoSettings from '../components/settings/UserInfoSettings';
 
 export default function UserSettings(): JSX.Element {
@@ -30,13 +30,9 @@ export default function UserSettings(): JSX.Element {
                     </SettingsLink>
                 </SettingsNavigationTop>
                 <SettingsNavigationBottom>
-                    <button
-                        className="group w-full border-transparent border-l-2 py-2 px-3 flex items-center text-sm font-medium text-white hover:bg-indigo-500 hover:bg-opacity-10 hover:border-l-2 hover:border-indigo-500 hover:border-opacity-50"
-                        onClick={executeLogout}
-                    >
-                        <LogoutIcon className="mr-3 h-6 w-6" aria-hidden="true" />
+                    <SettingsButton onClick={executeLogout} icon={LogoutIcon} isDanger={true}>
                         Logout
-                    </button>
+                    </SettingsButton>
                 </SettingsNavigationBottom>
             </SettingsNavigation>
             <SettingsContent onGoBack={goBack}>
