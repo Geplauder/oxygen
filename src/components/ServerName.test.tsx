65c38e69f418d7d0bafd7b64e1fd2d140b582a8a
Fabian Vowie <fabian@vowie.me>
2021-10-20 18:36:10 +0200

    Add tests for ChannelName and ServerName components

---
+++ src/components/ServerName.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { store } from '../app/store';
+import { render, screen } from '@testing-library/react';
+import faker from 'faker';
+import ServerName from './ServerName';
+import { Provider } from 'react-redux';
+import { addServer } from '../features/servers/serversSlice';
+import configureStore from 'redux-mock-store';
+
+describe('ServerName', () => {
+    it('renders server name', () => {
+        const serverName = faker.lorem.word();
+
+        store.dispatch(addServer({
+            id: faker.datatype.uuid(),
+            name: serverName,
+            owner_id: faker.datatype.uuid(),
+            created_at: faker.date.past().toString(),
+            updated_at: faker.date.past().toString(),
+        }));
+
+        render(
+            <Provider store={store}>
+                <ServerName />
+            </Provider>
+        );
+
+        const textElement = screen.getByText(new RegExp(serverName, "i"));
+
+        expect(textElement).toBeInTheDocument();
+    });
+
+    it('shows create channel dropdown item when user is owner', () => {
+        
+    });
+});
