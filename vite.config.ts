a34c33c57d01112aaa1a82fb7298e82178471e70
Fabian Vowie <fabian@vowie.me>
2021-10-20 18:34:47 +0200

    Add workaround for environment variables in tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()]
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        ['process.env.' + key]: `"${val}"`,
+      }
+    },
+    {},
+  );
+
+  return {
+    define: envWithProcessPrefix,
+    plugins: [...(process.env.NODE_ENV !== "test" ? [reactRefresh()] : [])]
+  }
+});
