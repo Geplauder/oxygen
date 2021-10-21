094287bb489fb9203632eb41e7ba8bc60b9f0e6a
Fabian Vowie <fabian@vowie.me>
2021-10-21 20:36:35 +0200

    Add test for AuthenticatedRoute component

---
+++ src/app/router.test.tsx
@@ -0,0 +1,53 @@
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { Router } from 'react-router';
+
+import { render, screen } from '../utility/testUtils';
+import { AuthenticatedRoute } from './router';
+
+describe('AuthenticatedRoute', () => {
+    it('does not show content when unauthenticated', async () => {
+        const history = createMemoryHistory();
+
+        render(
+            <Router history={history}>
+                <AuthenticatedRoute path='/'>
+                    foobar
+                </AuthenticatedRoute>
+            </Router>,
+            {
+                preloadedState: {
+                    auth: {
+                        token: null,
+                    }
+                }
+            }
+        );
+
+        expect(history.location.pathname).toBe('/login');
+        expect(screen.queryByText(/foobar/i)).not.toBeInTheDocument();
+    });
+
+    it('shows content when authenticated', async () => {
+        const history = createMemoryHistory();
+
+        render(
+            <Router history={history}>
+                <AuthenticatedRoute path='/'>
+                    foobar
+                </AuthenticatedRoute>
+            </Router>,
+            {
+                preloadedState: {
+                    auth: {
+                        token: 'foobar',
+                    }
+                }
+            }
+        );
+
+        expect(history.location.pathname).toBe('/');
+        expect(screen.getByText(/foobar/i)).toBeInTheDocument();
+    });
+});
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,7 +3,7 @@ import { Redirect, Route } from "react-router";
 import { selectToken } from "../features/auth/authSlice";
 import { useAppSelector } from "./hooks";
 
-export function AuthenticatedRoute({ children, path }: { children: React.ReactNode, path: string }): JSX.Element {
+export function AuthenticatedRoute({ children, path }: { children?: React.ReactNode, path: string }): JSX.Element {
     const token = useAppSelector(selectToken);
     return (
         <Route path={path} render={({ location }) => token ? (children) : (
