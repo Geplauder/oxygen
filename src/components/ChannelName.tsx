2cc88f8f9a0c88da802eb52b2ea13bec8b519d5a
Fabian Vowie <fabian@vowie.me>
2021-10-29 16:35:50 +0200

    Only show ServerName and ChannelName component if a server is selected

--- src/components/ChannelName.tsx
+++ src/components/ChannelName.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Channel } from '../types';
 
 export default function ChannelName({ selectedChannel }: { selectedChannel: Channel | null }): JSX.Element {
+    if (selectedChannel === null) {
+        return <div />;
+    }
+
     return (
         <div className="h-12 bg-main">
             <div className="flex items-center h-12 border-b border-main-black">
                 <p className='mx-4 text-white text-xl font-semibold select-none'>
                     <span># </span>
-                    {selectedChannel?.name}
+                    {selectedChannel.name}
                 </p>
             </div>
         </div>
