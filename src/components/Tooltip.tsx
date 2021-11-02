9fdf161a9698d4e19854e1cbe2a9d00d64fa19e6
Fabian Vowie <fabian@vowie.me>
2021-11-02 17:32:53 +0100

    Add verified icon to servers tooltip in ServerListItem component

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import React from 'react';
 import { Placement } from 'tippy.js';
 
-export default function Tooltip({ className = '', children, placement, content }: { className?: string, children: React.ReactNode, placement: Placement, content: string }): JSX.Element {
+export default function Tooltip({ className = '', children, placement, content }: { className?: string, children: React.ReactNode, placement: Placement, content: React.ReactNode }): JSX.Element {
     return (
         <Tippy placement={placement} render={attrs => (
             <div className={classNames('bg-main-black text-white font-bold px-4 py-2 rounded-lg', { 'hidden': attrs['data-reference-hidden'] })} tabIndex={-1} {...attrs}>
