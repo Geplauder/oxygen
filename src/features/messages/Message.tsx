c5393263baf7900a562125c52537e849b8c72ed8
Fabian Vowie <fabian@vowie.me>
2021-11-20 22:54:48 +0100

    Add user popover wrapper to message user and user list item

--- src/features/messages/Message.tsx
+++ src/features/messages/Message.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import moment from 'moment';
 import { Message as MessageObject } from '../../types';
 import UserAvatar from '../user/UserAvatar';
+import UserPopover from '../users/UserPopover';
 
 export default function Message({ message }: { message: MessageObject }): JSX.Element {
     return (
         <div className='flex space-x-4 px-4 py-2 hover:bg-indigo-500 hover:bg-opacity-10 border-l-2 border-transparent hover:border-indigo-500 hover:border-opacity-50'>
             <div>
-                <UserAvatar user={message.user} size='normal' />
+                <UserPopover user={message.user}>
+                    <UserAvatar user={message.user} size='normal' />
+                </UserPopover>
             </div>
             <div className='text-white'>
-                <p className='font-semibold'>
-                    {message.user.username} <span className='ml-1 font-normal text-[8pt] text-gray-300 cursor-default'>{moment(message.created_at).calendar()}</span>
-                </p>
+                <div className='inline-flex items-center'>
+                    <UserPopover user={message.user}>
+                        <span className='font-semibold hover:underline'>{message.user.username}</span>
+                    </UserPopover>
+                    <span className='ml-2 mt-1 font-normal text-[8pt] text-gray-300 cursor-default'>{moment(message.created_at).calendar()}</span>
+                </div>
                 <p className='break-anywhere'>
                     {message.content}
                 </p>
