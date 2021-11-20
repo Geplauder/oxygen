9e6ae5ac8dd66a25483c1e54192378e17f77d651
Fabian Vowie <fabian@vowie.me>
2021-11-20 23:08:10 +0100

    Add large size for user avatar component

--- src/features/user/UserAvatar.tsx
+++ src/features/user/UserAvatar.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Jdenticon from '../../components/Jdenticon';
 import { User } from '../../types';
 
-export default function UserAvatar({ className, user, size }: { className?: string, user: User, size: 'normal' | 'small' }): JSX.Element {
+export default function UserAvatar({ className, user, size }: { className?: string, user: User, size: 'large' | 'normal' | 'small' }): JSX.Element {
     return (
-        <Jdenticon value={user.id} className={classNames(className, 'rounded-full', { 'w-12 h-12': size === 'normal', 'w-10 h-10': size === 'small' })} />
+        <Jdenticon value={user.id} className={classNames(className, 'rounded-full', { 'w-16 h-16': size === 'large', 'w-12 h-12': size === 'normal', 'w-10 h-10': size === 'small' })} />
     );
 }
